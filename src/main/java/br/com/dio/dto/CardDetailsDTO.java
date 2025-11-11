de6715798dd33248e443b813d8929cb4d45e2762
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-10 23:43:03 -0300

    Update CardDetailsDTO.java

--- src/main/java/br/com/dio/dto/CardDetailsDTO.java
+++ src/main/java/br/com/dio/dto/CardDetailsDTO.java
@@ -2,14 +2,14 @@ package br.com.dio.dto;
 
 import java.time.OffsetDateTime;
 
-public record CardDetailsDTO(Long id,
-                             String title,
-                             String description,
-                             boolean blocked,
-                             OffsetDateTime blockedAt,
-                             String blockReason,
-                             int blocksAmount,
-                             Long columnId,
-                             String columnName
-) {
-}
+public record CardDetailsDTO(
+        Long id,
+        String title,
+        String description,
+        boolean blocked,
+        OffsetDateTime blockedAt,
+        String blockReason,
+        int blocksAmount,
+        Long columnId,
+        String columnName
+) {}
