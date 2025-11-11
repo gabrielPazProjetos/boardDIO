4fe3fd5f19decc82e1e39f2c0cb8fc5681c365c9
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-10 23:37:13 -0300

    Update CardEntity.java

--- src/main/java/br/com/dio/persistence/entity/CardEntity.java
+++ src/main/java/br/com/dio/persistence/entity/CardEntity.java
@@ -8,6 +8,7 @@ public class CardEntity {
     private Long id;
     private String title;
     private String description;
-    private BoardColumnEntity boardColumn = new BoardColumnEntity();
 
+    private BoardColumnEntity boardColumn = new BoardColumnEntity();
 }
+
