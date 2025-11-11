f07aa76d22f32f1fbbac10f997746adf46921191
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-10 23:28:56 -0300

    Update BoardDAO.java

--- src/main/java/br/com/dio/persistence/dao/BoardDAO.java
+++ src/main/java/br/com/dio/persistence/dao/BoardDAO.java
@@ -11,14 +11,15 @@ import java.util.Optional;
 @AllArgsConstructor
 public class BoardDAO {
 
-    private Connection connection;
+    private final Connection connection;
 
     public BoardEntity insert(final BoardEntity entity) throws SQLException {
-        var sql = "INSERT INTO BOARDS (name) values (?);";
-        try(var statement = connection.prepareStatement(sql)){
+        var sql = "INSERT INTO BOARDS (name) VALUES (?);";
+        try (var statement = connection.prepareStatement(sql)) {
             statement.setString(1, entity.getName());
             statement.executeUpdate();
-            if (statement instanceof StatementImpl impl){
+
+            if (statement instanceof StatementImpl impl) {
                 entity.setId(impl.getLastInsertID());
             }
         }
@@ -27,7 +28,7 @@ public class BoardDAO {
 
     public void delete(final Long id) throws SQLException {
         var sql = "DELETE FROM BOARDS WHERE id = ?;";
-        try(var statement = connection.prepareStatement(sql)){
+        try (var statement = connection.prepareStatement(sql)) {
             statement.setLong(1, id);
             statement.executeUpdate();
         }
@@ -35,27 +36,26 @@ public class BoardDAO {
 
     public Optional<BoardEntity> findById(final Long id) throws SQLException {
         var sql = "SELECT id, name FROM BOARDS WHERE id = ?;";
-        try(var statement = connection.prepareStatement(sql)){
+        try (var statement = connection.prepareStatement(sql)) {
             statement.setLong(1, id);
-            statement.executeQuery();
-            var resultSet = statement.getResultSet();
-            if (resultSet.next()){
+            var resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
                 var entity = new BoardEntity();
                 entity.setId(resultSet.getLong("id"));
                 entity.setName(resultSet.getString("name"));
                 return Optional.of(entity);
             }
-            return Optional.empty();
         }
+        return Optional.empty();
     }
 
     public boolean exists(final Long id) throws SQLException {
         var sql = "SELECT 1 FROM BOARDS WHERE id = ?;";
-        try(var statement = connection.prepareStatement(sql)){
+        try (var statement = connection.prepareStatement(sql)) {
             statement.setLong(1, id);
-            statement.executeQuery();
-            return statement.getResultSet().next();
+            var resultSet = statement.executeQuery();
+            return resultSet.next();
         }
     }
-
 }
