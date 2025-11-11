b8cb72283ba9a984a370474872a508956b267e41
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-10 23:28:14 -0300

    Update CardDAO.java

--- src/main/java/br/com/dio/persistence/dao/CardDAO.java
+++ src/main/java/br/com/dio/persistence/dao/CardDAO.java
@@ -15,59 +15,60 @@ import static java.util.Objects.nonNull;
 @AllArgsConstructor
 public class CardDAO {
 
-    private Connection connection;
+    private final Connection connection;
 
     public CardEntity insert(final CardEntity entity) throws SQLException {
-        var sql = "INSERT INTO CARDS (title, description, board_column_id) values (?, ?, ?);";
-        try(var statement = connection.prepareStatement(sql)){
-            var i = 1;
-            statement.setString(i ++, entity.getTitle());
-            statement.setString(i ++, entity.getDescription());
+        var sql = "INSERT INTO CARDS (title, description, board_column_id) VALUES (?, ?, ?);";
+        try (var statement = connection.prepareStatement(sql)) {
+            int i = 1;
+            statement.setString(i++, entity.getTitle());
+            statement.setString(i++, entity.getDescription());
             statement.setLong(i, entity.getBoardColumn().getId());
             statement.executeUpdate();
-            if (statement instanceof StatementImpl impl){
+
+            if (statement instanceof StatementImpl impl) {
                 entity.setId(impl.getLastInsertID());
             }
         }
         return entity;
     }
 
-    public void moveToColumn(final Long columnId, final Long cardId) throws SQLException{
+    public void moveToColumn(final Long columnId, final Long cardId) throws SQLException {
         var sql = "UPDATE CARDS SET board_column_id = ? WHERE id = ?;";
-        try(var statement = connection.prepareStatement(sql)){
-            var i = 1;
-            statement.setLong(i ++, columnId);
+        try (var statement = connection.prepareStatement(sql)) {
+            int i = 1;
+            statement.setLong(i++, columnId);
             statement.setLong(i, cardId);
             statement.executeUpdate();
         }
     }
 
     public Optional<CardDetailsDTO> findById(final Long id) throws SQLException {
-        var sql =
-                """
-                SELECT c.id,
-                       c.title,
-                       c.description,
-                       b.blocked_at,
-                       b.block_reason,
-                       c.board_column_id,
-                       bc.name,
-                       (SELECT COUNT(sub_b.id)
-                               FROM BLOCKS sub_b
-                              WHERE sub_b.card_id = c.id) blocks_amount
-                  FROM CARDS c
-                  LEFT JOIN BLOCKS b
-                    ON c.id = b.card_id
-                   AND b.unblocked_at IS NULL
-                 INNER JOIN BOARDS_COLUMNS bc
-                    ON bc.id = c.board_column_id
-                  WHERE c.id = ?;
-                """;
-        try(var statement = connection.prepareStatement(sql)){
+        var sql = """
+            SELECT c.id,
+                   c.title,
+                   c.description,
+                   b.blocked_at,
+                   b.block_reason,
+                   c.board_column_id,
+                   bc.name,
+                   (SELECT COUNT(sub_b.id)
+                      FROM BLOCKS sub_b
+                     WHERE sub_b.card_id = c.id) blocks_amount
+              FROM CARDS c
+              LEFT JOIN BLOCKS b
+                ON c.id = b.card_id
+               AND b.unblocked_at IS NULL
+             INNER JOIN BOARDS_COLUMNS bc
+                ON bc.id = c.board_column_id
+              WHERE c.id = ?;
+            """;
+
+        try (var statement = connection.prepareStatement(sql)) {
             statement.setLong(1, id);
-            statement.executeQuery();
-            var resultSet = statement.getResultSet();
-            if (resultSet.next()){
+            var resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
                 var dto = new CardDetailsDTO(
                         resultSet.getLong("c.id"),
                         resultSet.getString("c.title"),
@@ -84,5 +85,4 @@ public class CardDAO {
         }
         return Optional.empty();
     }
-
 }
