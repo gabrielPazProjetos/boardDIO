98fd609c280e5f133b9e6c38e637efddd8579ca5
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-10 23:51:40 -0300

    Update OffsetDateTimeConverter.java

--- src/main/java/br/com/dio/persistence/converter/OffsetDateTimeConverter.java
+++ src/main/java/br/com/dio/persistence/converter/OffsetDateTimeConverter.java
@@ -1,5 +1,21 @@
 package br.com.dio.persistence.converter;
 
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+public class OffsetDateTimeConverter {
+
+    public static OffsetDateTime toOffsetDateTime(Timestamp timestamp) {
+        return timestamp == null ? null : timestamp.toInstant().atOffset(ZoneOffset.UTC);
+    }
+
+    public static Timestamp toTimestamp(OffsetDateTime offsetDateTime) {
+        return offsetDateTime == null ? null : Timestamp.from(offsetDateTime.toInstant());
+    }
+}
+package br.com.dio.persistence.converter;
+
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
